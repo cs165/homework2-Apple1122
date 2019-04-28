492dbaca06e26af04d1ffa7be9335609a855b285
Kyle Huang <kyle.huang168@gmail.com>
2019-04-29 00:09:16 +0800

    part1 finished

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -12,9 +12,22 @@ const MATCH_LIST = {
 
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+  if(node.nodeType === Node.TEXT_NODE)
+  {
+    for(var key in MATCH_LIST) // https://stackoverflow.com/questions/3068534/getting-javascript-object-key-list
+    {
+      node.textContent = node.textContent.replace(key, MATCH_LIST[key]);
+      console.log(node.textContent);
+    }
+    
+  }
+
+  for(const nodeChild of node.childNodes) // https://developer.mozilla.org/zh-TW/docs/Web/API/Node/childNodes
+    transformTextNodes(nodeChild);
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+// console.log(MATCH_LIST[0]);
--- script.js
+++ script.js
@@ -2,3 +2,144 @@
 
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
+const result = document.querySelector('.result');
+const choiceDiv = document.querySelectorAll('.choice-grid div');
+
+
+var oneTime = false;
+var twoTime = false;
+var threeTime = false;
+
+var ans = ["", "", ""];
+
+for(const div of choiceDiv)
+{
+    // var id = div.dataset.questionId;
+    div.addEventListener("click", function(){
+        click(this);
+    });
+    // console.log(id);
+}
+
+// function: click()
+function click(div)
+{
+    var id = div.dataset.questionId;
+
+    switch(id)
+    {
+        case "one":
+            checkOne(div.dataset.choiceId);
+            break;
+        case "two":
+            checkTwo(div.dataset.choiceId);
+            break;
+        case "three":
+            checkThree(div.dataset.choiceId);
+            break;
+
+    }
+}
+
+function checkOne(choiceId)
+{
+    var oneGroup = document.querySelectorAll('div[data-question-id="one"]');
+    ans[0] = choiceId;
+    // console.log(ans);
+    oneTime = true;
+
+    for(var i of oneGroup)
+    {
+        if(i.getAttribute('data-choice-id') === choiceId)
+        {
+            i.querySelector(".checkbox").src = "images/checked.png";
+            i.style.backgroundColor = "#cfe3ff";
+            i.style.opacity = "1";
+            // console.log(i);
+        }
+        else
+        {
+            i.querySelector(".checkbox").src = "images/unchecked.png";
+            i.style.backgroundColor = "#f4f4f4";
+            i.style.opacity = "0.6";
+        }
+    }
+
+    if(oneTime && twoTime && threeTime)
+        getFinalResult();
+    // console.log("one: " + choiceId);
+}
+
+function checkTwo(choiceId)
+{
+    var twoGroup = document.querySelectorAll('div[data-question-id="two"');
+    ans[1] = choiceId;
+    twoTime = true;
+
+    for(var i of twoGroup)
+    {
+        if(i.getAttribute('data-choice-id') === choiceId)
+        {
+            i.querySelector(".checkbox").src = "images/checked.png";
+            i.style.backgroundColor = "#cfe3ff";
+            i.style.opacity = "1";
+        }
+        else
+        {
+            i.querySelector(".checkbox").src = "images/unchecked.png";
+            i.style.backgroundColor = "#f4f4f4";
+            i.style.opacity = "0.6";
+        }
+    }
+
+    if(oneTime && twoTime && threeTime)
+        getFinalResult();
+    // console.log("two: " + choiceId);
+}
+
+function checkThree(choiceId)
+{
+    var threeGroup = document.querySelectorAll('div[data-question-id="three"');
+    ans[2] = choiceId;
+    threeTime = true;
+
+    for(var i of threeGroup)
+    {
+        if(i.getAttribute('data-choice-id') === choiceId)
+        {
+            i.querySelector(".checkbox").src = "images/checked.png";
+            i.style.backgroundColor = "#cfe3ff";
+            i.style.opacity = "1";
+        }
+        else
+        {
+            i.querySelector(".checkbox").src = "images/unchecked.png";
+            i.style.backgroundColor = "#f4f4f4";
+            i.style.opacity = "0.6";
+        }
+    }
+    // console.log("three: " + choiceId);
+    if(oneTime && twoTime && threeTime)
+        getFinalResult();
+}
+
+function getFinalResult()
+{
+    var index = ans[0];
+
+    if(ans[1] === ans[2])
+        index = ans[1];
+
+    result.querySelector(".title").innerHTML = "You got: " + RESULTS_MAP[index]["title"];
+    result.querySelector(".contents").innerHTML = "You got: " + RESULTS_MAP[index]["contents"];
+    
+    result.style.display = "block";
+    // console.log(index);
+    // console.log("final: " + ans);
+}
+
+function restartQuiz()
+{
+    window.scrollTo(0, 0);
+    window.location.reload();
+}
